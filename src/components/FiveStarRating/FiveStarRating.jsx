4c0f2c07231e9b8294085c97f07f3c4b5e0519a8
Guillaume Melenchon <guillaume@MacBook-Air-de-Guillaume.local>
2023-03-24 15:16:01 +0100

    add stars by score

---
+++ src/components/FiveStarRating/FiveStarRating.jsx
@@ -0,0 +1,25 @@
+import style from "./style.module.css";
+import { StarFill, Star as StarEmpty, StarHalf } from "react-bootstrap-icons";
+
+export function FiveStarRating({ rating }) {
+  // Déclarer un tableau vide
+  const starList = [];
+  // Nbr d'étoiles pleine
+  const starFillCount = Math.floor(rating);
+  // Nbr d'étoiles a moitié
+  const hasStarHalf = rating - starFillCount >= 0.5;
+  // Nbr d'étoiles vide
+  const emptyStarCount = 5 - starFillCount - (hasStarHalf ? 1 : 0);
+
+  console.log(starFillCount, hasStarHalf, emptyStarCount);
+  for (let i = 1; i <= starFillCount; i++) {
+    starList.push(<StarFill key={"start-fill" + i} />);
+  }
+  if (hasStarHalf) {
+    starList.push(<StarHalf key={"star-half"} />);
+  }
+  for (let i = 1; i <= emptyStarCount; i++) {
+    starList.push(<StarEmpty key={"start-empty" + i} />);
+  }
+  return <div>{starList}</div>;
+}
--- src/components/TVShowDetail/TVShowDetails.jsx
+++ src/components/TVShowDetail/TVShowDetails.jsx
@@ -1,10 +1,16 @@
+import { FiveStarRating } from "../FiveStarRating/FiveStarRating";
 import style from "./style.module.css";
 
 export function TVShowDetail({ tvShow }) {
+  const rating = tvShow.vote_average / 2;
+
   return (
     <div>
       <div className={style.title}>{tvShow.name}</div>
-      <div className={style.rating}>{tvShow.vote_average / 2}</div>
+      <div className={style.rating_container}>
+        <FiveStarRating rating={rating} />
+        <div className={style.rating}>{rating}</div>
+      </div>
       <div className={style.overview}>{tvShow.overview}</div>
     </div>
   );
