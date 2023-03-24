d61c00c20a623032fb5b1658e4bbddfae22c0a91
Guillaume Melenchon <guillaume@MacBook-Air-de-Guillaume.local>
2023-03-24 14:45:54 +0100

    tvshow component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+import { TVShowAPI } from "./api/tv-show";
 import "./global.css";
 import style from "./style.module.css";
+import { BACKDROP_BASE_URL } from "./config";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetails";
 
 export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  async function fectchPopulars() {
+    const populars = await TVShowAPI.fetchPopulars();
+    if (populars.length > 0) {
+      setCurrentTVShow(populars[0]);
+    }
+  }
+
+  useEffect(() => {
+    fectchPopulars();
+  }, []);
+
   return (
-    <div className={style.main_container}>
+    <div
+      className={style.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
       <div className={style.header}>
         <div className="row">
           <div className="col-4">
@@ -15,7 +39,9 @@ export function App() {
           </div>
         </div>
       </div>
-      <div className={style.tv_show_details}>Details</div>
+      <div className={style.tv_show_details}>
+        {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
+      </div>
       <div className={style.recommendations}>Recommendations</div>
     </div>
   );
